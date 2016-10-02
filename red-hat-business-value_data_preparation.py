8a295db74d7120f9335fc49c9402ef3838f97ba3
nitish11 <nitish.bhardwaj11@gmail.com>
2016-10-02 14:43:53 +0530

    Redhat Business Value contest submission

--- red-hat-business-value_data_preparation.py
+++ red-hat-business-value_data_preparation.py
@@ -48,11 +48,16 @@ train_data = pd.merge(activity_train_data, people_data, how='outer', on=None, le
 # print train_data.head(5)
 
 #Fill missing data
-train_data = train_data.fillna(0)
+#train_data = train_data.fillna(0)
+predictors = ["people_id","activity_id","activity_category","char_1_x","char_2_x","char_3_x", "char_4_x","char_5_x","char_6_x","char_7_x","char_8_x","char_9_x", "char_10_x","char_1_y","group_1","char_2_y","char_3_y","char_4_y","char_5_y","char_11","char_12","char_13","char_14" ,"char_15","char_16","char_17","char_18","char_19","char_20","char_21","char_22","char_23", "char_24","char_25","char_26","char_27","char_28","char_29","char_30","char_31","char_32", "char_33","char_34","char_35","char_36","char_37","char_38"]
+
+for feature in predictors:
+    print "Updating ",feature
+    train_data[feature] = train_data[feature].fillna(train_data[feature].median())
 # print train_data.head(5)
 
-#Replace False with 0 and True with 1
 
+#Replace False with 0 and True with 1
 
 #Change non_numeric data to numeric data 
 train_data["people_id"] = [convert_data(x,'_') for x in train_data["people_id"]]
@@ -77,6 +82,7 @@ train_data.drop("date_x",axis=1,inplace=True)
 train_data.drop("date_y",axis=1,inplace=True)
 print train_data.head(5)
 
+#Dumping the train data
 pickle.dump(train_data,open('train_data.p','wb'))
 
 print('-'*30)
